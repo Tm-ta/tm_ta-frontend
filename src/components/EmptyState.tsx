ef56936eaf19b0582f375c965d0aaaa1a70af9e5
Nayoung Jeong <skduddkekffu@naver.com>
2025-11-29 01:37:16 +0900

    fix

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 export default function EmptyState({ text }: { text: string }) {
   return (
-    <div style={{ textAlign:'center' }} className="center">
+    <div style={{ textAlign:'center', height:'100%', justifyContent:'center' }} className="center">
       { text == '아직 일정이 없어요' ?
         <div>
           <h3> 약속 생성 완료!</h3> 
-          <div className="gray">{text}<br/>{"가능한 시간을 먼저 등록해주세요!"}</div>
+          <div className="gray">가능한 시간을 먼저 등록하고<br/>{"친구들에게 공유해보세요!"}</div>
         </div>
         :
 
